65b680378ca588e5736ae826fa83084aec116bcb
SmilesAir <ryan@smilesair.com>
2019-08-31 15:58:22 -0700

    Adding invokes per second to express server

--- server/app.js
+++ server/app.js
@@ -26,11 +26,11 @@ app.use(cors({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
 
 app.use('/', awsRouter);
 
--- server/routes/aws.js
+++ server/routes/aws.js
@@ -4,15 +4,35 @@ const router = express.Router()
 const DataManager = require("../source/dataManager.js")
 const Common = require("complete-judging-common/source/backendCommon.js")
 
+let invokeCountHistory = []
+let historyCount = 10
+let historyIndex = 0
+let invokeCount = 0
+setInterval(() => {
+    invokeCountHistory[historyIndex++ % historyCount] = invokeCount
+    invokeCount = 0
+
+    let avg = 0
+    for (let c of invokeCountHistory) {
+        avg += c
+    }
+    avg /= invokeCountHistory.length
+
+    //console.log(`Avg invoke count: ${avg.toFixed(2)} per second`)
+}, 500);
+
 router.post("/tournamentName/:tournamentName/importTournamentDataFromAWS", async (req, res) => {
+    ++invokeCount
     res.json(await DataManager.importTournamentDataFromAWS(req.params.tournamentName))
 })
 
 router.get("/tournamentName/:tournamentName/requestTournamentInfoFromServer", async (req, res) => {
+    ++invokeCount
     res.json(await DataManager.getTournamentInfo(req.params.tournamentName))
 })
 
 router.post("/tournamentName/:tournamentName/exportTournamentDataToAWS", async (req, res) => {
+    ++invokeCount
     await DataManager.exportTournamentDataToAWS(req.params.tournamentName)
 
     res.json({
@@ -21,10 +41,12 @@ router.post("/tournamentName/:tournamentName/exportTournamentDataToAWS", async (
 })
 
 router.get("/tournamentName/:tournamentName/getPlayingPool", async (req, res) => {
+    ++invokeCount
     res.json(await Common.getActivePool(req.params.tournamentName, req.query.isAlt === "true"))
 })
 
 router.get("/tournamentName/:tournamentName/divisionIndex/:divisionIndex/roundIndex/:roundIndex/poolIndex/:poolIndex/getPoolResults", async (req, res) => {
+    ++invokeCount
     let data = await Common.getPoolResults(req.params.tournamentName,
         req.params.divisionIndex,
         req.params.roundIndex,
@@ -33,6 +55,7 @@ router.get("/tournamentName/:tournamentName/divisionIndex/:divisionIndex/roundIn
 })
 
 router.post("/reportJudgeScore", async (req, res) => {
+    ++invokeCount
     if (req.body.tournamentName !== undefined &&
         req.body.judgeId !== undefined &&
         req.body.results !== undefined) {
@@ -50,6 +73,7 @@ router.post("/reportJudgeScore", async (req, res) => {
 })
 
 router.post("/tournamentName/:tournamentName/setPlayingPool", async (req, res) => {
+    ++invokeCount
     if (req.body.data !== undefined) {
         await Common.setPlayingPool(req.params.tournamentName, req.body.data)
     }
@@ -64,6 +88,7 @@ router.post("/tournamentName/:tournamentName/setPlayingPool", async (req, res) =
 })
 
 router.post("/tournamentName/:tournamentName/setJudgeState", async (req, res) => {
+    ++invokeCount
     await Common.setJudgeState(req.params.tournamentName, req.body.judgeId, req.body.status, req.body.isAlt)
 
     res.json({
@@ -72,6 +97,7 @@ router.post("/tournamentName/:tournamentName/setJudgeState", async (req, res) =>
 })
 
 router.post("/tournamentName/:tournamentName/divisionIndex/:divisionIndex/roundIndex/:roundIndex/poolIndex/:poolIndex/clearPoolResults", async (req, res) => {
+    ++invokeCount
     let data = await Common.clearPoolResults(req.params.tournamentName,
         req.params.divisionIndex,
         req.params.roundIndex,
@@ -80,6 +106,7 @@ router.post("/tournamentName/:tournamentName/divisionIndex/:divisionIndex/roundI
 })
 
 router.post("/tournamentName/:tournamentName/stopPlayingPools", async (req, res) => {
+    ++invokeCount
     let data = await Common.stopPlayingPools(req.params.tournamentName)
     res.json(data)
 })
